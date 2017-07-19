ba3723468ec97d35eb615c71cac9658d4db77254
km <1658649930@qq.com>
2017-07-19 21:40:26 +0800

    说明
    配置文件

--- movies/movies/Common/Conf/config.php
+++ movies/movies/Common/Conf/config.php
@@ -1,14 +1,5 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
-	'DB_TYPE'               =>  '',     // 数据库类型
-    'DB_HOST'               =>  '', // 服务器地址
-    'DB_NAME'               =>  '',          // 数据库名
-    'DB_USER'               =>  '',      // 用户名
-    'DB_PWD'                =>  '',          // 密码
-    'DB_PORT'               =>  '',        // 端口
-    'DB_PREFIX'             =>  '',    // 数据库表前缀
-
     'DB_TYPE'=>'mysql',// 数据库类型
 	'DB_HOST'=>'127.0.0.1',// 服务器地址
 	'DB_NAME'=>'wordblog',// 数据库名
@@ -18,5 +9,5 @@ return array(
 	'DB_PREFIX'=>'km_',// 数据库表前缀
 	'DB_CHARSET'=>'utf8',// 数据库字符集
 
-	
+
 );
